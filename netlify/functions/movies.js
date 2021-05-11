ed8fec0576db8b68d3c7db1a60a1402fcb69df0e
ferrona <ferrona.lie@kellogg.northwestern.edu>
2021-05-10 19:32:36 -0500

    FINISH

--- netlify/functions/movies.js
+++ netlify/functions/movies.js
@@ -5,13 +5,13 @@ let csv = require('neat-csv')
 let fs = require('fs')
 
 // defines a lambda function
-exports.handler = async function(event) {
+exports.handler = async function (event) {
   // write the event object to the back-end console
   console.log(event)
 
   // read movies CSV file from disk
   let moviesFile = fs.readFileSync(`./movies.csv`)
-  
+
   // turn the movies file into a JavaScript object, wait for that to happen
   let moviesFromCsv = await csv(moviesFile)
 
@@ -22,20 +22,27 @@ exports.handler = async function(event) {
   let year = event.queryStringParameters.year
   let genre = event.queryStringParameters.genre
 
-  // create a new array to be returned by the API
-  let moviesToReturn = {
-    count: 0,
-    information: []
+  if (year == undefined || genre == undefined || genre == `` || year == ``) {
+    return {
+      statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+      body: `Nope!` // a string of data -- make up an error message
+    }
   }
+  else {
+    // create a new array to be returned by the API
+    let moviesToReturn = {
+      numResults: 0,
+      movies: []
+    }
 
     // loop through the posts; for each one:
-    for (let i=0; i < moviesFromCsv.length; i++) {
+    for (let i = 0; i < moviesFromCsv.length; i++) {
 
       // store each listing in memory
       let movie = moviesFromCsv[i]
 
       // check if the year is not zero. if so:
-      if (movie.startYear == year) {
+      if (movie.startYear == year && movie.genres.includes(genre) && movie.genres != `\\N` && movie.runtimeMinutes != `\\N`) {
         // create a new post object containing the pertinent fields
         let theMovie = {
           title: movie.primaryTitle,
@@ -43,30 +50,16 @@ exports.handler = async function(event) {
           movieGenre: movie.genres
         }
 
-      // add (push) the post object to the final Array
-      moviesToReturn.information.push(theMovie)
-      moviesToReturn.count = moviesToReturn.count + 1
+        // add (push) the post object to the final Array
+        moviesToReturn.movies.push(theMovie)
+        moviesToReturn.numResults = moviesToReturn.numResults + 1
       }
-
-      // if (year == undefined || genre == undefined) {
-      //   return {
-      //     statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-      //     body: `Nope!` // a string of data
-      //   }
-      // }
-    //   else {
-    //     let  = noMovies = {
-    //       numResults: 0,
-    //       movies: []
-    //     }
-    //   // add (push) the post object to the final Array
-    //   moviesToReturn.push(noMovies)
-    // }
+    }
 
     // a lambda function returns a status code and a string of data
     return {
       statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
       body: JSON.stringify(moviesToReturn)// a string of data
     }
-  }
+  } // else statement ends
 }
